1b48d8f74a32f2c527b94c76f851f2d428622eef
redcamellia <nasashinega@gmail.com>
2016-08-03 18:02:11 +0530

    Added reception sign up

--- HMS/MainWindow.xaml.cs
+++ HMS/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.CodeDom;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
 using Telerik.Windows;
 using Telerik.Windows.Controls;
 
@@ -16,34 +16,17 @@ namespace HMS
         public MainWindow()
         {
             InitializeComponent();
-            ShowInTaskbar(this, "Reception Login");
-            //            this.Icon = new Image()
-            //            {
-            //                Source = new BitmapImage(new Uri("pack://application:,,,/hms.png"))
-            //            };
         }
 
         private void FadeIn(object sender, EventArgs e)
         {
             var fadeAnimation = new DoubleAnimation { From = 0, Duration = new Duration(TimeSpan.FromSeconds(3)) };
-            //            var unravelAnimation = new DoubleAnimation {From = 0, Duration = new Duration(TimeSpan.FromSeconds(2.5))};
-            //            EmailLabel.BeginAnimation(WidthProperty, unravelAnimation);
-            //            PwordLabel.BeginAnimation(WidthProperty, unravelAnimation);
             MailTextBox.BeginAnimation(OpacityProperty, fadeAnimation);
             PasswordBox.BeginAnimation(OpacityProperty, fadeAnimation);
             UnameTextBlock.BeginAnimation(OpacityProperty, fadeAnimation);
             PasswordTextBlock.BeginAnimation(OpacityProperty, fadeAnimation);
             logButton.BeginAnimation(OpacityProperty, fadeAnimation);
-        }
-
-        public static void ShowInTaskbar(RadWindow control, string title)
-        {
-            control.Show();
-            var window = control.ParentOfType<Window>();
-            window.ShowInTaskbar = true;
-            window.Title = title;
-            var uri = new Uri("pack://application:,,,/hms.png");
-            window.Icon = BitmapFrame.Create(uri);
+            RecLoginLabel.BeginAnimation(OpacityProperty, fadeAnimation);
         }
 
         private void PasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e)
@@ -60,7 +43,8 @@ namespace HMS
 
         private void LogButton_OnActivate(object sender, RadRoutedEventArgs e)
         {
-            this.Close();
+            var blah = new DocLog();
+            blah.Show();
         }
     }
 }
